c14cb26a755076e9731ffec2c7030e6cf5d0f37c
Jonathan May <jonathan.may@devmountain.com>
2019-02-13 18:57:19 -0600

    Refactor edit state out of redux

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,89 +1,80 @@
 import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { connect } from "react-redux";
-import { toggleEdit, queueUpdate } from "../redux/reducers";
+import styled from "styled-components";
+import { queueUpdate } from "../redux/reducers";
 
-//needs refactoring
+const isEqual = (a: String | Date, b: String) =>
+  a instanceof Date ? a.toISOString() === b : a == b;
 
 const Cell = (props: any) => {
-  const inputRef = useRef({} as any);
   let [content, updateContent] = useState(props.row[props.column]);
+  const [edit, toggleEdit] = useState(false);
+
   if (content instanceof Date) {
     content = content.toISOString();
   }
-  useEffect(
-    () => {
-      if (!props.updates.length) {
-        updateContent(props.row[props.column]);
-      }
-    },
-    [props.updates.length]
-  );
 
-  useLayoutEffect(
-    () => {
-      if (inputRef.current && inputRef.current.focus) {
-        inputRef.current.focus();
-      }
-    },
-    [props.editing]
-  );
-
-  if (props.editing === props.row + props.column + props.index) {
-    return (
-      <td>
-        <input
-          ref={inputRef}
-          value={content ? content.toString() : "NULL"}
-          onBlur={() => {
-            if (content !== props.row[props.column]) {
-              props.queueUpdate({
-                original: { ...props.row },
-
-                updated: { ...props.row, [props.column]: content }
-              });
-            }
-            props.toggleEdit("");
-          }}
-          onKeyPress={e => {
-            if (e.key === "Enter") {
-              inputRef.current.blur();
-            }
-          }}
+  return (
+    <Td onDoubleClick={() => toggleEdit(true)}>
+      {edit ? (
+        <Input
+          value={content === "" ? "" : content || "NULL"}
           onChange={e => {
             updateContent(e.target.value);
           }}
-          style={{
-            background:
-              content !== props.row[props.column] && props.updates.length
-                ? "yellow"
-                : "",
-            width: "100%",
-            padding: 0,
-            boxSizing: "border-box",
-            border: "1px solid gray"
+          autoFocus={true}
+          onBlur={() => {
+            if (!isEqual(props.row[props.column], content)) {
+              props.queueUpdate({
+                original: { ...props.row[props.column] },
+                updated: {
+                  ...props.row[props.column],
+                  [props.column]: content
+                }
+              });
+            }
+            toggleEdit(false);
           }}
         />
-      </td>
-    );
-  } else {
-    return (
-      <td
-        style={{
-          background:
-            content != props.row[props.column] &&
-            !(props.row[props.column] instanceof Date)
-              ? "yellow"
-              : ""
-        }}
-        onClick={() => props.toggleEdit(props.row + props.column + props.index)}
-      >
-        {content ? content.toString() : "NULL"}
-      </td>
-    );
-  }
+      ) : (
+        <Article
+          edited={!isEqual(props.row[props.column], content)}
+          null={!content}
+        >
+          {content || "NULL"}
+        </Article>
+      )}
+    </Td>
+  );
 };
+
+const Article = styled.article`
+  background: ${props => (props.edited ? "rgb(250, 250, 200)" : "")};
+  color: ${props => (props.null ? "gray" : "black")};
+  font-size: ${props => (props.null ? ".9em" : "1em")};
+`;
+
+const Td = styled.td`
+  border: 1px solid rgba(222, 222, 222, 1);
+`;
+
+const Input = styled.input`
+  :active,
+  :focus {
+    width: 100%;
+    box-sizing: content-box;
+    border: none;
+    // outline: yellow;
+    // outline-offset: 2px;
+    background: white;
+  }
+  width: 100%;
+  border: none;
+  padding: 0;
+  font-size: 1em;
+`;
 
 export default connect(
   state => state,
-  { toggleEdit, queueUpdate }
+  { queueUpdate }
 )(Cell);
