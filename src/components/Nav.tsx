125debd9a13fac0f240e13b2096ea0c58a11aeee
Jonathan May <jonathan.may@devmountain.com>
2019-02-13 18:57:59 -0600

    Add minor style updates

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,16 +5,16 @@ import { NavLink } from "react-router-dom";
 const Nav = props => {
   return (
     <>
-      <h5>Tables</h5>
+      <h5 style={{ padding: "5px 10px", margin: 0 }}>Tables</h5>
       {props.db.table_structure.map(table => {
         return (
           <NavLink
-            activeStyle={{ background: "gray" }}
+            activeStyle={{ background: "rgb(219,219,219)" }}
             to={`/tables/${table.table_name}`}
             style={{
               textDecoration: "none",
               padding: "5px 10px",
-              color: "blue"
+              color: "black"
             }}
           >
             {table.table_name}
